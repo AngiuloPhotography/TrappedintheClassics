935b7b1c33fb64ae1074be92654367f7879ee23c
AngiuloPhotography <angiulophotography@gmail.com>
2025-06-22 08:57:14 -0500

    Add files via upload

---
+++ 5jQ7yAPgv6o_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":10126,"text":"Oh delightful!"},{"start":11405,"text":"I can see them already!"}]}]};
+    window.globalLoadJsAsset('story_content/5jQ7yAPgv6o_transcripts.js', JSON.stringify(data));
+})();
